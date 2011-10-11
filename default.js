93544e2ffe2d69a119c1ce555b8e79df7811438f
John Lam <jlam@iunknown.com>
2011-10-11 12:08:12 -0700

    added interactive execution, multiple output windows, key bindings for toggling output windows

--- default.js
+++ default.js
@@ -1,3 +1,8 @@
+var console = null;
+var rafael = null;
+var editor = null;
+var html = null;
+
 instance_eval = (function () {
     var $ = {};
     return function (str) {
@@ -9,6 +14,29 @@ instance_eval = (function () {
         }
     }
 })();
+
+// This toggles between the three different output windows
+show_output = function(id) {
+    var top = id * 700;
+    var output = document.getElementById("output");
+    output.scrollTop = top;
+}
+
+// Helper functions for the interactive console
+cls = function() {
+    console.getSession().setValue("");
+}
+
+print = function(str) {
+    console.navigateFileEnd();
+    console.insert("\n" + str);
+}
+
+dir = function(obj) {
+    for (key in obj) {
+        print(key);
+    }
+}
 
 execute_selection = function(editor, env, args, request) {
     var doc = editor.getSession().doc;
@@ -46,7 +74,7 @@ execute_selection = function(editor, env, args, request) {
 }
 
 init_editor = function() {
-    var editor = ace.edit("editor");
+    editor = ace.edit("editor");
     editor.setTheme("ace/theme/vibrant_ink");
     
     var JavaScriptMode = require("ace/mode/javascript").Mode;
@@ -58,20 +86,67 @@ init_editor = function() {
         name: 'execute',
         bindKey: { 
             win: 'Ctrl-Return',
-            mac: 'Command-Return',
+            mac: 'Ctrl-Return',
             sender: 'editor'
         },
         exec: function(env, args, request) {
             execute_selection(editor, env, args, request);
         }
     });
+    canon.addCommand({
+        name: 'show_rafael',
+        bindKey: { 
+            win: 'Ctrl-1',
+            mac: 'Ctrl-1',
+            sender: 'editor'
+        },
+        exec: function(env, args, request) {
+            show_output(0);
+        }
+    });
+    canon.addCommand({
+        name: 'show_console',
+        bindKey: { 
+            win: 'Ctrl-2',
+            mac: 'Ctrl-2',
+            sender: 'editor'
+        },
+        exec: function(env, args, request) {
+            show_output(1);
+        }
+    });
+    canon.addCommand({
+        name: 'show_html1',
+        bindKey: { 
+            win: 'Ctrl-3',
+            mac: 'Ctrl-3',
+            sender: 'editor'
+        },
+        exec: function(env, args, request) {
+            show_output(2);
+        }
+    });
 }
 
 init_rafael = function() {
     rafael = Raphael("rafael");
-j
+}
+
+init_console = function() {
+    console = ace.edit("console");
+    console.setTheme("ace/theme/vibrant_ink");
+    console.setShowPrintMargin(false);
+    console.renderer.setHScrollBarAlwaysVisible(false);
+}
+
+init_html = function() {
+    html = document.getElementById("html1");
+    html.innerHTML = "<h1>change me!</h1>";
+}
 
 window.onload = function() {
     init_editor();
     init_rafael();
+    init_console();
+    init_html();
 }
