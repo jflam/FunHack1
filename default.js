f04e43c5a1858e2cd7b496f787244c095255e0d7
John Lam <jlam@iunknown.com>
2011-10-10 22:12:06 -0700

    binding ctrl-enter / command-enter to execute code interactively

--- default.js
+++ default.js
@@ -1,27 +1,77 @@
+instance_eval = (function () {
+    var $ = {};
+    return function (str) {
+        try {
+            return eval(str);
+        }
+        catch (e) {
+            return e;
+        }
+    }
+})();
+
+execute_selection = function(editor, env, args, request) {
+    var doc = editor.getSession().doc;
+    var selection = editor.getSelectionRange();
+    var code = null;
+    if (selection.isEmpty()) {
+        // Note that we handle the non-selection case specially.
+        // First we see if we are at the end of the line, if so we run the code on the current line.
+        // If we are not at the end of the line, CTRL-ENTER will move to the end of the line. This has
+        // the effect of always executing the code on the line after two consecutive CTRL-ENTERS.
+        var s = editor.getSelection();
+        var p1 = s.getCursor();
+        s.moveCursorLineEnd();
+        var p2 = s.getCursor();
+        if (p1.column === p2.column) {
+            code = doc.getLine(p1.row);
+        }
+    } else {
+        code = doc.getTextRange(selection);
+    }
+    if (code !== null) {
+        var result = instance_eval(code);
+        // insert the output below the end of the selection
+        var pos = editor.selection.getRange().end;
+        editor.clearSelection();
+        editor.selection.moveCursorToPosition(pos);
+        if (result !== null && result !== undefined) {
+            if (typeof(result) === "function") {
+                editor.insert("\r\n//> created function(s)\r\n");
+            } else {
+                editor.insert("\r\n//> " + result.toString() + "\r\n");
+            }
+        }
+    }
+}
+
 init_editor = function() {
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/vibrant_ink");
     
     var JavaScriptMode = require("ace/mode/javascript").Mode;
     editor.getSession().setMode(new JavaScriptMode());
+    editor.renderer.setHScrollBarAlwaysVisible(false);
+
+    var canon = require('pilot/canon')
+    canon.addCommand({
+        name: 'execute',
+        bindKey: { 
+            win: 'Ctrl-Return',
+            mac: 'Command-Return',
+            sender: 'editor'
+        },
+        exec: function(env, args, request) {
+            execute_selection(editor, env, args, request);
+        }
+    });
 }
 
 init_rafael = function() {
-    // Creates canvas 320 × 200 at 10, 50
-    var paper = Raphael("rafael");
-
-    // Creates circle at x = 50, y = 40, with radius 10
-    var circle = paper.circle(50, 40, 10);
-    // Sets the fill attribute of the circle to red (#f00)
-    circle.attr("fill", "#f00");
-
-    // Sets the stroke attribute of the circle to white
-    circle.attr("stroke", "#fff");
-}
+    rafael = Raphael("rafael");
+j
 
 window.onload = function() {
     init_editor();
     init_rafael();
 }
-
-
